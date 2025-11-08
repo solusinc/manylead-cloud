9d60465360d6a3e1d1cffb2e5da19065fb2be30c
solusinc <soluscodes@gmail.com>
2025-11-07 23:19:28 -0300

    refactor(apps): rename nextjs to dashboard and remove T3 boilerplate

---
+++ apps/dashboard/src/app/page.tsx
@@ -0,0 +1,17 @@
+import { HydrateClient, prefetch, trpc } from "~/trpc/server";
+
+export default function HomePage() {
+  prefetch(trpc.post.all.queryOptions());
+
+  return (
+    <HydrateClient>
+      <main className="container h-screen py-16">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            Create <span className="text-primary">T3</span> Turbo
+          </h1>
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
